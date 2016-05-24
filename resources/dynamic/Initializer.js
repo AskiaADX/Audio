715ce7047cd0f9c2477892456ea7048dd175ec0b
jeromed <jeromed@askia.com>
2016-05-24 18:14:55 +0200

    Added IEMobile
    Added IEMobile

--- resources/dynamic/Initializer.js
+++ resources/dynamic/Initializer.js
@@ -5,6 +5,6 @@
         javascriptSupport: {%= Browser.Support("javascript") %},
         controls: {%= On(CurrentADC.PropValue("controls") <> "true",0,1) %},
         autoplay: {%= On(CurrentADC.PropValue("autoplay") <> "true",0,1) %},
-        mobile: {%= On((CurrentADC.PropValue("controls") = "false" and ((Browser.Mobile = true) or (Browser.UserAgent.IndexOf("iPad") >= 1) or (Browser.UserAgent.IndexOf("iPod") >= 1) or (Browser.UserAgent.IndexOf("iPhone") >= 1) or (Browser.UserAgent.IndexOf("Android") >= 1) )) <> true,0,1) %}
+        mobile: {%= On((CurrentADC.PropValue("controls") = "false" and ((Browser.Mobile = true) or (Browser.UserAgent.IndexOf("iPad") >= 1) or (Browser.UserAgent.IndexOf("iPod") >= 1) or (Browser.UserAgent.IndexOf("iPhone") >= 1) or (Browser.UserAgent.IndexOf("Android") >= 1) or (Browser.UserAgent.IndexOf("IEMobile") >= 1) )) <> true,0,1) %}
     });
 } ());
